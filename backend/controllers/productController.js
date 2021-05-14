100cc1c5e570dd535a39b31d2a2dc5e01deddad0
Vanshaj Kapoor <vanshaj.kapoor@indiamart.com>
2021-05-14 11:22:07 +0530

    Create and edit  product by admin

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -45,7 +45,7 @@ const createProduct = asyncHandler(async (req, res) => {
     name: 'Sample Name',
     price: 0,
     user: req.user._id,
-    image: '/images/sample.jpeg',
+    image: '/images/sample.jpg',
     brand: 'Sample Brand',
     category: 'Sample Category',
     countInStock: 0,
