482e2f34ebe680fc4aba41b23395ce501c40d45c
Vanshaj Kapoor <vanshaj.kapoor@indiamart.com>
2021-05-14 20:09:10 +0530

    Show all orders list to admin

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -47,7 +47,7 @@ const Header = () => {
               )}
               {userInfo && userInfo.isAdmin && (
                 <NavDropdown title="Admin" id="adminmenu">
-                  <LinkContainer to="admin/userlist">
+                  <LinkContainer to="/admin/userlist">
                     <NavDropdown.Item>Users</NavDropdown.Item>
                   </LinkContainer>
                   <LinkContainer to="/admin/productlist">
