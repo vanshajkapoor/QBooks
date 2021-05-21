9c4e33056171b0a4bff5ca2f29dc43385146d5e5
Vanshaj Kapoor <vanshaj.kapoor@indiamart.com>
2021-05-21 17:19:11 +0530

    Bug fix : remove items from cart

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -2,6 +2,7 @@ import {
   CART_ADD_ITEM,
   CART_SAVE_SHIPPING_ADDRESS,
   CART_SAVE_PAYMENT_METHOD,
+  CART_REMOVE_ITEM,
 } from '../constants/cartConstants';
 
 export const cartReducer = (
@@ -27,6 +28,11 @@ export const cartReducer = (
           cartItems: [...state.cartItems, item],
         };
       }
+    case CART_REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((x) => x.product !== action.payload),
+      };
 
     case CART_SAVE_SHIPPING_ADDRESS:
       return {
